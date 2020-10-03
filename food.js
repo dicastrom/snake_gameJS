e3379b9d3ae8cecb3e0a954610b7413eaef000df
Diego Castro <51303820+dicastrom@users.noreply.github.com>
2020-10-03 15:41:14 -0400

    Add files via upload
    Initial Commit. Game works and the following functionality is implemented: snake can move up ,down, left and right. Snake can ear "food" and will grow bigger when food is eaten. Snake cannot go over itself or go out of bounds (Game over otherwise). Possible future additions: score counter, speed increase with time or size. Hopefully one of the many retro games I make!

---
+++ food.js
@@ -0,0 +1,36 @@
+import { onSnake, expandSnake} from './snake.js'
+import { randomGridPosition } from './grid.js';
+
+let food = randomGridPosition()
+const EXPANSION_RATE = 1
+
+export function update() 
+{
+    if(onSnake(food)){
+        expandSnake(EXPANSION_RATE)
+        food = randomGridPosition()
+    }
+
+
+
+}
+
+export function draw(gameBoard){
+    
+        const foodElement = document.createElement('div')
+        foodElement.style.gridRowStart = food.y
+        foodElement.style.gridColumnStart = food.x
+        foodElement.classList.add('food')
+        gameBoard.appendChild(foodElement)
+
+}
+
+function getRandomFoodPostion()
+{
+    while(newFoodPostion ==null || onSnake(newFoodPostion))
+    {
+        newFoodPostion = randomGridPosition()
+    }
+    return newFoodPostion
+
+}
